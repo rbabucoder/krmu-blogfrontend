b1e9e07031fccf8cb6f903b51380089d88af4e8e
= <=>
2025-11-23 15:28:34 +0530

    try again

--- app/(blogs)/(all-blogs)/comp/CommonBlogHeroSection.tsx
+++ app/(blogs)/(all-blogs)/comp/CommonBlogHeroSection.tsx
@@ -4,7 +4,11 @@ type Props = {
 
 const CommonBlogHeroSection = ({ catName }: Props) => {
   return (
-    <section className="pt-[140px] pb-[10%] md:py-[14%] bg-[url(/mainBlogBanner.webp)] px-4 bg-cover bg-no-repeat bg-center">
+    <section className="pt-[140px] pb-[10%] md:py-[14%] px-4 bg-cover bg-no-repeat bg-center" style={{
+      background: `url(/mainBlogBanner.webp)`,
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}>
       <div className="max-w-[1600px] mx-auto w-full">
         <h1
           className="text-white font-bold text-4xl sm:text-[42px] lg:text-[90px] leading-[1.2]"
