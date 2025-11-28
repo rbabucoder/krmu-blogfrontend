6489dfe495689b64a1ddce07dd5bcf025023fdbb
= <=>
2025-11-28 16:23:09 +0530

    subdomain add

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // output: "export",
   // basePath: "/blog3",
-  assetPrefix: "https://www.krmangalam.edu.in/blog",
+  // assetPrefix: "https://www.krmangalam.edu.in/blog",
   images: {
     remotePatterns: [
       {
